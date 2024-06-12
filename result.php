dc3486cb5b65ee961a473285baee65091b66dd2f
michele laptop <micheledong2803@gmail.com>
2024-06-12 15:34:37 +0200

    m2 - password generated

--- result.php
+++ result.php
@@ -1,8 +1,10 @@
 <?php
 $passwordLen = $_GET["passwordLen"];
+$charArray = array_merge(range('A', 'Z'), range('a', 'z'));
+
 if($passwordLen>0){
     for($i = 0; $i<$passwordLen; $i++){
-    echo "X";
+    echo $charArray[rand(0,51)];
 }
 }else{
     echo "Devi inserire un numero positivo!";
